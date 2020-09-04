fbfb499aae3f3d14f50be6a80db3c8aa99c26a2a
brianmaleek <maleekmalik@gmail.com>
2020-09-05 02:51:57 +0300

    Understanding Android Applications Basics: 3: Understanding Activities and Activity Layout Interaction:- 2.11: Populating Spinner and Migration to AndroidX. Course no.

--- app/src/main/java/com/brma/notekeeper/NoteActivity.java
+++ app/src/main/java/com/brma/notekeeper/NoteActivity.java
@@ -1,11 +1,9 @@
 package com.brma.notekeeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
