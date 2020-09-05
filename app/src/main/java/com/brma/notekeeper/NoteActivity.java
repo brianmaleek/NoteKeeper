1e39b653355a4e6c963cd0d8f7febf0d6042dfa3
brianmaleek <maleekmalik@gmail.com>
2020-09-06 01:05:50 +0300

    Understanding Android Applications Basics: 5: Taking a deeper look at Activity Interaction (late bindings, Implicit intents):- 5.11: Implementing Note change cancellation.

--- app/src/main/java/com/brma/notekeeper/NoteActivity.java
+++ app/src/main/java/com/brma/notekeeper/NoteActivity.java
@@ -23,6 +23,7 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteText;
     private Spinner mSpinnerCourses;
     private int mNotePosition;
+    private boolean mIsCancelling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +52,13 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        saveNote();
+        if (mIsCancelling) {
+            if (mIsNewNote)
+                DataManager.getInstance().removeNote(mNotePosition);
+
+        } else {
+            saveNote();
+        }
     }
 
     private void saveNote() {
@@ -98,6 +105,10 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail ();
             return true;
+
+        } else if (id == R.id.action_cancel) {
+            mIsCancelling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
