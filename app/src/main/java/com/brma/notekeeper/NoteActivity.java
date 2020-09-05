6785dbdd4a56e61635b4a96d0698402df947f016
brianmaleek <maleekmalik@gmail.com>
2020-09-05 03:17:43 +0300

    Understanding Android Applications Basics: 4: Working with Activities:- 4.4: Handling ListView selection with an Anonymous class.

--- app/src/main/java/com/brma/notekeeper/NoteActivity.java
+++ app/src/main/java/com/brma/notekeeper/NoteActivity.java
@@ -7,7 +7,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -20,8 +23,11 @@ public class NoteActivity extends AppCompatActivity {
 
         Spinner spinnerCourses = findViewById(R.id.spinner_courses);
 
-
-
+        List <CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter <CourseInfo> adapterCourses = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
 
     }
 
