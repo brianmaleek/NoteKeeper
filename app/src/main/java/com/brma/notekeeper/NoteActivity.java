f7b640acfd98a2560b308297532a593793d3324b
brianmaleek <maleekmalik@gmail.com>
2020-09-05 23:37:53 +0300

    Understanding Android Applications Basics: 4: Working with Activities:- 4.11: Handling new note creation.

--- app/src/main/java/com/brma/notekeeper/NoteActivity.java
+++ app/src/main/java/com/brma/notekeeper/NoteActivity.java
@@ -15,7 +15,8 @@ import android.widget.Spinner;
 import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
-    public static final String NOTE_INFO = "com.brma.notekeeper.NOTE_INFO";
+    public static final String NOTE_POSITION = "com.brma.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
     private boolean mIsNewNote;
 
@@ -53,8 +54,10 @@ public class NoteActivity extends AppCompatActivity {
 
     private void readDisplayStateValues() {
         Intent intent = getIntent();
-        mNote = intent.getParcelableExtra(NOTE_INFO);
-        mIsNewNote = mNote == null;
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+        mIsNewNote = position == POSITION_NOT_SET;
+        if (!mIsNewNote)
+            mNote = DataManager.getInstance().getNotes().get(position);
     }
 
     @Override
