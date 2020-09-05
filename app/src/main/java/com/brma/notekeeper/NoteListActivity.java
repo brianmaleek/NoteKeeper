4728419d0f6b4f0ee35fc9bd3e64aaf50e948391
brianmaleek <maleekmalik@gmail.com>
2020-09-05 21:46:53 +0300

    Understanding Android Applications Basics: 4: Working with Activities:- 4.10: Handling an Intent and displaying the content

--- app/src/main/java/com/brma/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/brma/notekeeper/NoteListActivity.java
@@ -1,5 +1,6 @@
 package com.brma.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -36,11 +38,23 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     private void initializeDisplayContent() {
-        ListView listNotes = findViewById(R.id.list_notes);
+        final ListView listNotes = findViewById(R.id.list_notes);
 
         List <NoteInfo> notes = DataManager.getInstance().getNotes();
         ArrayAdapter <NoteInfo> adapterNotes = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, notes);
         listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_INFO, note);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
