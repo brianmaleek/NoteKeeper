555d918ba354bc8c80f0bde7d58d5dc6b9e8c72a
brianmaleek <maleekmalik@gmail.com>
2020-09-06 21:01:03 +0300

    Understanding Android Applications Basics: 6: Understanding Activity Life Cycle (Activity lifecycle):- 6.8: Saving Activity state

---
+++ app/src/main/java/com/brma/notekeeper/NoteActivityViewModel.java
@@ -0,0 +1,10 @@
+package com.brma.notekeeper;
+
+import androidx.lifecycle.ViewModel;
+
+public class NoteActivityViewModel extends ViewModel {
+    public String  mOriginalNoteCourseId;
+    public String mOriginalNoteTitle;
+    public String mOriginalNoteText;
+
+}
